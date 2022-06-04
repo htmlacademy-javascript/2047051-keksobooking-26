45cc72b40565645212de49bbfdd1fc663e766f91
andreev-oe <andreev.oe@yandex.ru>
2022-06-04 14:19:12 +0300

    реализованы функции возвращающие случайное целое число (getRandomInteger) и число с указанным кол-вом знаков после запятой (getRandomFloat)

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const getRandomInteger = (scaleLow, scaleHigh) => {
+  if (scaleLow === scaleHigh) {
+    return scaleLow;
+  } else if (scaleLow < 0 || scaleLow > scaleHigh) {
+    return 'Введите корректный диапазон';
+  }
+  const range = scaleHigh - scaleLow;
+  return Math.round(Math.random()*range+scaleLow);
+};
+
+getRandomInteger(5, 10);
+
+const getRandomFloat = (scaleLow, scaleHigh, digitsAfterDecimalPoint) => {
+  const range = scaleHigh - scaleLow;
+  if (scaleLow === scaleHigh) {
+    return scaleLow;
+  } else if (scaleLow < 0 || scaleLow > scaleHigh) {
+    return 'Введите корректный диапазон';
+  } else if (digitsAfterDecimalPoint === undefined) {
+    return Math.round(Math.random()*range+scaleLow);
+  }
+  const floatRange = Math.random()*range+scaleLow;
+  return floatRange.toPrecision(digitsAfterDecimalPoint+1);
+};
+
+getRandomFloat(5, 10, 4);
