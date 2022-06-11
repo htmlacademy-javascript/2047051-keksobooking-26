b6ef3b3e3561784928c2fb84450bde5661ddbab4
andreev-oe <andreev.oe@yandex.ru>
2022-06-11 18:49:51 +0300

    Оптимизировал названия переменных. Вместо повторяющегося блока кода сделал вызов функции getRandomInteger внутри getRandomFloat при кол-ве знаков после запятой равному 0

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 // Данные
 
 const RENT_OFFERS_AMOUNT = 10;
-const titlesList = ['Эконом класс', 'Ниже среднего', 'В целом неплохо', 'Хорошечно', 'Отлично', 'Божественно'];
-const rentEstateTypesList = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkinTimeList = ['12:00', '13:00', '14:00'];
-const checkoutTimeList = ['12:00', '13:00', '14:00'];
-const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const description = ['Не предложение, а сказка', 'Может и получше найдёте','С собаками не пускаем','Пиво на завтрак и ужин','Есть парковочное место'];
-const photosList = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const titles = ['Эконом класс', 'Ниже среднего', 'В целом неплохо', 'Хорошечно', 'Отлично', 'Божественно'];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinTimes = ['12:00', '13:00', '14:00'];
+const checkoutTimes = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const descriptions = ['Не предложение, а сказка', 'Может и получше найдете','С собаками не пускаем','Пиво на завтрак и ужин','Есть велопарковка'];
+const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+let avatarIdCount = 1; // Число с которого начнется отсчет аватаров
 
 // Функции
 
@@ -17,26 +18,19 @@ const getRandomInteger = (scaleLow, scaleHigh) => {
   } else if (scaleLow < 0 || scaleLow > scaleHigh) {
     throw new RangeError ('Диапазон меньше 0 или отрицательное начальное значание');
   }
-  const range = scaleHigh - scaleLow;
-  return Math.round(Math.random()*range+scaleLow);
+  return Math.round(Math.random()*(scaleHigh - scaleLow)+scaleLow);
 };
 
 const getRandomFloat = (scaleLow, scaleHigh, digitsAfterDecimalPoint = 0) => {
-  const range = scaleHigh - scaleLow;
-  if (scaleLow === scaleHigh) {
-    return scaleLow;
-  } else if (scaleLow < 0 || scaleLow > scaleHigh) {
-    throw new RangeError ('Диапазон меньше 0 или отрицательное начальное значание');
+  if (digitsAfterDecimalPoint === 0) {
+    return getRandomInteger(scaleLow,scaleHigh);
   } else if (digitsAfterDecimalPoint < 0) {
     throw new RangeError ('Количество знаков после запятой не может быть меньше 0');
-  } else if (digitsAfterDecimalPoint === 0) {
-    return Math.round(Math.random()*range+scaleLow);
   }
-  const floatRange = Math.random()*range+scaleLow;
-  return Number(floatRange.toFixed(digitsAfterDecimalPoint));
+  return Number((Math.random()*(scaleHigh - scaleLow)+scaleLow).toFixed(digitsAfterDecimalPoint));
 };
 
-const getArrayPhotos = (defaultArrayOfElements) => {
+const getPhotos = (defaultArrayOfElements) => {
   const newArrayOfElements = [];
   for (let i = 0; i < getRandomInteger(3,10); i++) {
     newArrayOfElements.push(defaultArrayOfElements[getRandomInteger(0,defaultArrayOfElements.length-1)]);
@@ -46,44 +40,43 @@ const getArrayPhotos = (defaultArrayOfElements) => {
 
 const getRandomFeatures = () => {
   const randomFeaturesArray = [];
-  for (let i = 0; i < getRandomInteger(0, featuresList.length); i++) {
-    const randomNumber = getRandomInteger(0, featuresList.length-1);
+  for (let i = 0; i < getRandomInteger(0, features.length); i++) {
+    const randomNumber = getRandomInteger(0, features.length-1);
     let countDuplicates = 0;
     if (randomFeaturesArray.length < 1) {
-      randomFeaturesArray.push(featuresList[randomNumber]);
+      randomFeaturesArray.push(features[randomNumber]);
     } else {
       for (let j = 0 ; j < randomFeaturesArray.length; j++) {
-        if (randomFeaturesArray[j] === featuresList[randomNumber]) {
+        if (randomFeaturesArray[j] === features[randomNumber]) {
           countDuplicates++;
         }
       }
       if (countDuplicates === 0) {
-        randomFeaturesArray.push(featuresList[randomNumber]);
+        randomFeaturesArray.push(features[randomNumber]);
       }
     }
   }
   return randomFeaturesArray;
 };
 
-let avatarIdCount = 1;
 
-const createRentInfo = () => {
+const CreateRentInfo = () => {
   const RentInfoTemplate = {
     author: {
       avatar: 0,
     },
     offer: {
-      title: titlesList[getRandomInteger(0, titlesList.length-1)],
+      title: titles[getRandomInteger(0, titles.length-1)],
       address: 0,
       price: getRandomInteger(500, 5000),
-      type: rentEstateTypesList[getRandomInteger(0, rentEstateTypesList.length-1)],
+      type: types[getRandomInteger(0, types.length-1)],
       rooms: getRandomInteger(1, 5),
       guests: getRandomInteger(1, 5),
-      checkin: checkinTimeList[getRandomInteger(0, checkinTimeList.length-1)],
-      checkout: checkoutTimeList[getRandomInteger(0, checkoutTimeList.length-1)],
+      checkin: checkinTimes[getRandomInteger(0, checkinTimes.length-1)],
+      checkout: checkoutTimes[getRandomInteger(0, checkoutTimes.length-1)],
       features: getRandomFeatures(),
-      description: description[getRandomInteger(0, description.length-1)],
-      photos: getArrayPhotos(photosList),
+      description: descriptions[getRandomInteger(0, descriptions.length-1)],
+      photos: getPhotos(photos),
     },
     location: {
       lat: getRandomFloat(35.65, 35.7, 5),
@@ -95,5 +88,4 @@ const createRentInfo = () => {
   avatarIdCount++;
   return RentInfoTemplate;
 };
-
-const rentOffersList = Array.from({length: RENT_OFFERS_AMOUNT}, createRentInfo);
+const rentOffers = Array.from({length: RENT_OFFERS_AMOUNT}, CreateRentInfo);
