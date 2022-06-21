eaceb8f66b3e36f3e26bdd0e960a8fb3f48bd43c
andreev-oe <andreev.oe@yandex.ru>
2022-06-21 11:32:34 +0300

    прибрал хлам

--- js/create-dom-elements.js
+++ js/create-dom-elements.js
@@ -7,42 +7,48 @@ import {
 const cardTemplate = document.querySelector('#card').content;
 const parentNode = cardTemplate.querySelector('.popup');
 const offersFragment = document.createDocumentFragment();
+const getRoomEnding = (count) => count > 1 ? 'комнаты' : 'комната';
+const getGuestEnding = (count) => count > 1 ? 'гостей' : 'гостя';
 
 const createCardsInDom = (offers) => {
   offers.forEach((offerPost) => {
     const newOfferinDom = parentNode.cloneNode(true);
+
     if (offerPost.offer.title === undefined) {
       newOfferinDom.querySelector('.popup__title').textContent = undefined;
       newOfferinDom.querySelector('.popup__title').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__title').textContent = offerPost.offer.title;
     }
+
     if (offerPost.offer.address === undefined) {
       newOfferinDom.querySelector('.popup__text--address').textContent = undefined;
       newOfferinDom.querySelector('.popup__text--address').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__text--address').textContent = offerPost.offer.address;
     }
+
     if (offerPost.offer.price === undefined) {
       newOfferinDom.querySelector('.popup__text--price').textContent = undefined;
       newOfferinDom.querySelector('.popup__text--price').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__text--price').textContent = `${offerPost.offer.price} ₽/ночь`;
     }
+
     if (offerPost.offer.type === undefined) {
       newOfferinDom.querySelector('.popup__type').textContent = undefined;
       newOfferinDom.querySelector('.popup__type').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__type').textContent = getRussianTypesNames(offerPost.offer.type);
     }
+
     if (offerPost.offer.description === undefined) {
       newOfferinDom.querySelector('.popup__description').textContent = undefined;
       newOfferinDom.querySelector('.popup__description').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__description').textContent = offerPost.offer.description;
     }
-    const getRoomEnding = (count) => count > 1 ? 'комнаты' : 'комната';
-    const getGuestEnding = (count) => count > 1 ? 'гостей' : 'гостя';
+
     if (offerPost.offer.rooms === undefined && offerPost.offer.guests === undefined) {
       newOfferinDom.querySelector('.popup__text--capacity').textContent = undefined;
       newOfferinDom.querySelector('.popup__text--capacity').classList.add('hidden');
@@ -53,6 +59,7 @@ const createCardsInDom = (offers) => {
     } else {
       newOfferinDom.querySelector('.popup__text--capacity').textContent = `${offerPost.offer.rooms} ${getRoomEnding(offerPost.offer.rooms)} для ${offerPost.offer.guests} ${getGuestEnding(offerPost.offer.guests)}`;
     }
+
     if (offerPost.offer.checkin === undefined && offerPost.offer.checkout === undefined) {
       newOfferinDom.querySelector('.popup__text--time').textContent = undefined;
       newOfferinDom.querySelector('.popup__text--time').classList.add('hidden');
@@ -63,22 +70,26 @@ const createCardsInDom = (offers) => {
     } else {
       newOfferinDom.querySelector('.popup__text--time').textContent = `Заезд после ${offerPost.offer.checkin}, выезд до ${offerPost.offer.checkout}`;
     }
+
     if (offerPost.author.avatar === undefined) {
       newOfferinDom.querySelector('.popup__avatar').src = undefined;
       newOfferinDom.querySelector('.popup__avatar').classList.add('hidden');
     } else {
       newOfferinDom.querySelector('.popup__avatar').src = offerPost.author.avatar;
     }
+
     if (offerPost.offer.photos === undefined) {
       newOfferinDom.querySelector('.popup__photos').classList.add('hidden');
     } else {
       setPhotoSrc(newOfferinDom.querySelector('.popup__photos'),offerPost.offer.photos);
     }
+
     if (offerPost.offer.features === undefined) {
       newOfferinDom.querySelector('.popup__features').classList.add('hidden');
     } else {
       getFeaturesAsDomElements(newOfferinDom.querySelectorAll('.popup__feature'),offerPost.offer.features);
     }
+
     offersFragment.append(newOfferinDom);
   });
 };
